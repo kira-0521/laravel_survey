d376938db134b43feaffefd79d5a1848243dcc11
kira-0521 <kiratanaka0521@gmail.com>
2022-02-19 12:56:53 +0900

    add: 格ページのUI追加

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -10,12 +10,5 @@ const store = useStore(key);
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
