b630c3c7c75d0789ec831c72a21dac01af5b23bb
kira-0521 <kiratanaka0521@gmail.com>
2022-02-19 13:59:35 +0900

    add: デフォルトレイアウト追加

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,10 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import Surveys from "../views/Surveys.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
 
 const routes = [
-    { path: "/", name: "Dashboard", component: Dashboard },
+    {
+        path: "/",
+        redirect: "/dashboard",
+        component: DefaultLayout,
+        children: [
+            { path: "/dashboard", name: "Dashboard", component: Dashboard },
+            { path: "/surveys", name: "Surveys", component: Surveys },
+        ],
+    },
     { path: "/login", name: "Login", component: Login },
     { path: "/register", name: "Register", component: Register },
 ];
