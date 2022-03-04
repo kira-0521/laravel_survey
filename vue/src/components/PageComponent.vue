d8947401bef142d923109733a8b9e33013c53fc0
kira-0521 <kiratanaka0521@gmail.com>
2022-03-04 22:08:30 +0900

    add: 汎用パーツとしてページコンポーネントの作成。

---
+++ vue/src/components/PageComponent.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+interface Props {
+  title: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <header class="bg-white shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold text-gray-900">{{ title }}</h1>
+      <slot name="header"></slot>
+    </div>
+  </header>
+  <main>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 sm:px-0">
+        <slot></slot>
+      </div>
+    </div>
+  </main>
+</template>
--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,68 +1,24 @@
-<template>
-  <div class="min-h-full">
-    <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
-      </div>
-    </header>
-    <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div class="px-4 py-6 sm:px-0">Content goes here</div>
-      </div>
-    </main>
-  </div>
-</template>
-
-<script lang="ts">
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-} from "@headlessui/vue";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/vue/outline";
-
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
-
-export default {
-  components: {
-    Disclosure,
-    DisclosureButton,
-    DisclosurePanel,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems,
-    BellIcon,
-    MenuIcon,
-    XIcon,
-  },
-  setup() {
-    return {
-      user,
-      navigation,
-      userNavigation,
-    };
-  },
-};
+<script lang="ts" setup>
+import PageComponent from "../components/PageComponent.vue";
 </script>
+
+<template>
+  <PageComponent title="Surveys">
+    <template v-slot:header>
+      <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+      <!-- <router-link
+        :to="{ name: 'SurveyCreate' }"
+        class="
+          py-2
+          px-3
+          text-white
+          bg-emerald-500
+          rounded-md
+          hover:bg-emerald-600
+        "
+      >
+        Add new Survey
+      </router-link> -->
+    </template>
+  </PageComponent>
+</template>
