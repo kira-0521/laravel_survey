b8c3788c8b8837d5beadf9a491f67467eb767746
kira-0521 <kiratanaka0521@gmail.com>
2022-03-06 18:11:16 +0900

    add: vuexからリプレイス用にpiniaセットアップ

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,11 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
-import { store, key } from "./store";
 import router from "./router/index";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
-
-app.use(store, key);
+app.use(createPinia());
 app.use(router);
 app.mount("#app");
