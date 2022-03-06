52b4b2c9c0f70838a56d9aff0363af608476d712
kira-0521 <kiratanaka0521@gmail.com>
2022-03-06 23:36:16 +0900

    update: tokenの参照を動的に変更できるようgettersに変更

--- vue/src/store/user.ts
+++ vue/src/store/user.ts
@@ -16,14 +16,20 @@ export const useUserStore = defineStore("user", {
     state: () => ({
         user: {
             data: {
-                name: "tanaka",
-                email: "email",
-                imageUrl:
-                    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+                name: "",
+                email: "",
+                imageUrl: "",
             },
             token: "",
         },
     }),
+    getters: {
+        token(state) {
+            return state.user.token
+                ? state.user.token
+                : sessionStorage.getItem("TOKEN");
+        },
+    },
     actions: {
         setUser(userData: { token: string; user: UserData }) {
             this.user.token = userData.token;
