935eeecd1b85e6bef00995e387aa1905f6e975e0
kira-0521 <kiratanaka0521@gmail.com>
2022-03-10 20:58:23 +0900

    add: 入力欄全て追加

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -27,7 +27,7 @@ const saveSurvey = () => console.log(model.value);
         </h1>
       </div>
     </template>
-    <form @submit.prevent="saveSurvey">
+    <form @submit.prevent="saveSurvey" v-if="model">
       <div class="shadow sm:rounded-md sm:overflow-hidden">
         <!-- Survey Fields -->
         <div class="py-4 px-6 bg-white space-y-6 sm:p-6">
@@ -38,7 +38,7 @@ const saveSurvey = () => console.log(model.value);
             </label>
             <div class="mt-1 flex items-center">
               <img
-                v-if="model?.image"
+                v-if="model.image"
                 :src="model.image"
                 :alt="model.title"
                 class="w-64 h-48 object-cover"
@@ -111,11 +111,110 @@ const saveSurvey = () => console.log(model.value);
             </div>
           </div>
           <!-- /Image -->
+
+          <!-- Title -->
+          <div>
+            <label for="title" class="block text-sm font-medium text-gray-700">
+              Title
+            </label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              v-model="model.title"
+              autocomplete="survey_title"
+              class="
+                mt-1
+                focus:ring-indigo-500 focus:border-indigo-500
+                block
+                w-full
+                shadow-sm
+                sm:text-sm
+                border-gray-300
+                rounded-md
+              "
+            />
+          </div>
+          <!-- /Title -->
+
+          <!-- Description -->
+          <div>
+            <label for="about" class="block text-sm font-medium text-gray-700">
+              Description
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              rows="3"
+              v-model="model.description"
+              autocomplete="survey_description"
+              placeholder="Describe_your_survey"
+              class="
+                shadow-sm
+                focus:ring-indigo-500 focus:border-indigo-500
+                block
+                w-full
+                shadow-sm
+                sm:text-sm
+                border-gray-300
+                rounded-md
+              "
+            ></textarea>
+          </div>
+          <!-- /Description -->
+
+          <!-- Expire Date -->
+          <div>
+            <label
+              for="expire_date"
+              class="block text-sm font-medium text-gray-700"
+              >Expire Date</label
+            >
+            <input
+              type="date"
+              name="expire_date"
+              id="expire_date"
+              v-model="model.expire_date"
+              class="
+                mt-1
+                focus:ring-indigo-500 focus:border-indigo-500
+                block
+                w-full
+                shadow-sm
+                sm:text-sm
+                border-gray-300
+                rounded-md
+              "
+            />
+          </div>
+          <!--/ Expire Date -->
+
+          <!-- Status -->
+          <div class="flex items-start">
+            <div class="flex items-center h-5">
+              <input
+                id="status"
+                name="status"
+                type="checkbox"
+                v-model="model.status"
+                class="
+                  focus:ring-indigo-500
+                  h-4
+                  w-4
+                  text-indigo-600
+                  border-gray-300
+                  rounded
+                "
+              />
+            </div>
+            <div class="ml-3 text-sm">
+              <label for="status" class="font-medium text-gray-700"
+                >Active</label
+              >
+            </div>
+          </div>
+          <!--/ Status -->
         </div>
-        <!-- Title -->
-        <!-- /Title -->
-        <!-- Description -->
-        <!-- /Description -->
         <!-- /Survey Fields -->
       </div>
     </form>
