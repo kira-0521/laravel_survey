cfc7dcceb1638911da1a0c374a19b6aef9ec796c
kira-0521 <kiratanaka0521@gmail.com>
2022-03-10 20:10:49 +0900

    add: Survey詳細画面の画像と変更ボタン作成

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -1,10 +1,124 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { useRoute } from "vue-router";
 import PageComponent from "../components/PageComponent.vue";
+import { useSurveyStore } from "../store/survey";
+import { Survey } from "../types/tmpSurveys";
+
+const route = useRoute();
+const surveys = useSurveyStore();
+
+let model = ref<Survey>();
+if (route.params.id) {
+  model.value = surveys.surveys.find(
+    (s) => s.id === parseInt((route.params as { id: string }).id)
+  );
+}
+
+const saveSurvey = () => console.log(model.value);
 </script>
 
 <template>
-  <PageComponent title="View or create a survey">
-    View or create a survey
+  <PageComponent>
+    <template v-slot:header>
+      <div class="flex items-center justify-between">
+        <h1 class="text-3xl font-bold text-gray-900">
+          {{ model?.id ? model.title : "Create as Survey" }}
+        </h1>
+      </div>
+    </template>
+    <form @submit.prevent="saveSurvey">
+      <div class="shadow sm:rounded-md sm:overflow-hidden">
+        <!-- Survey Fields -->
+        <div class="py-4 px-6 bg-white space-y-6 sm:p-6">
+          <!-- Image -->
+          <div>
+            <label class="block text-sm font-medium text-gray-700">
+              Image
+            </label>
+            <div class="mt-1 flex items-center">
+              <img
+                v-if="model?.image"
+                :src="model.image"
+                :alt="model.title"
+                class="w-64 h-48 object-cover"
+              />
+              <span
+                v-else
+                class="
+                  flex
+                  items-center
+                  justify-center
+                  h-12
+                  w-12
+                  rounded-full
+                  overflow-hidden
+                  bg-gray-100
+                "
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-[80%] w-[80%] text-gray-300"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+              </span>
+              <button
+                type="button"
+                class="
+                  relative
+                  overflow-hidden
+                  ml-5
+                  bg-white
+                  py-2
+                  px-3
+                  border border-gray-300
+                  rounded-md
+                  shadow-sm
+                  text-sm
+                  leading-4
+                  font-medium
+                  text-gray-700
+                  hover:bg-gray-50
+                  focus:outline-none
+                  focus:ring-2
+                  focus:ring-offset-2
+                  focus:ring-indigo-500
+                "
+              >
+                <input
+                  type="file"
+                  class="
+                    absolute
+                    left-0
+                    top-0
+                    right-0
+                    bottom-0
+                    opacity-0
+                    cursor-pointer
+                  "
+                />
+                Change
+              </button>
+            </div>
+          </div>
+          <!-- /Image -->
+        </div>
+        <!-- Title -->
+        <!-- /Title -->
+        <!-- Description -->
+        <!-- /Description -->
+        <!-- /Survey Fields -->
+      </div>
+    </form>
   </PageComponent>
 </template>
 
